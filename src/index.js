454fc725918ebaca596dad75c0ae95b9fc6d5d6b
Niv Tikolski <niv.tikolski@devalore.com>
2021-07-11 10:03:37 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const app = express()
+const port = 5000
+
+
+app.use(cookieParser());
+
+// set a cookie
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  res.cookie('hello', 'world');
+  next(); // <-- important!
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  // console.log(`Example app listening at http://localhost:${port}`)
+
+  // const logger = new seq.Logger({serverUrl: 'http://10.10.102.188:5341/'})
+
+  // logger.emit({
+  //   timestamp: new Date(),
+  //   level: "Information",
+  //   messageTemplate: 'Hello for the {n}th time, {user}!',
+  //   properties: {
+  //       user: 'admin',
+  //       n: 20
+  //   }
+  // });
+
+  // logger.close();
+})
+
+// http://localhost:5341/
