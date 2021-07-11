ee3931b52b40537fc8d03d36a69f49ae15174fe4
Niv Tikolski <niv.tikolski@devalore.com>
2021-07-11 11:39:35 +0300

    semek

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ const cookieParser = require('cookie-parser');
 const app = express()
 const port = 5000
 
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+console.log(results);
+
 
 app.use(cookieParser());
 
@@ -25,7 +44,11 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).json({hello: "world"});
+})
+
+app.post('/a', (req, res) => {
+  res.status(200).json({opa:"shlopa"});
 })
 
 app.listen(port, () => {
